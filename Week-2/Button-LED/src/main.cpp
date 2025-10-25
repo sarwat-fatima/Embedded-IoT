aabab0fb961b3601d787f1d951de6c941d7e89a1
Sarwat Fatima <sarwatfatima928@gmail.com>
2025-10-26 02:12:13 +0500

    Button-LED

---
+++ Week-2/Button-LED/src/main.cpp
@@ -0,0 +1,17 @@
+#include <Arduino.h>
+/*
+Name: Sarwat Fatima
+Reg no.: 23-NTU-CS-1092
+Class: BSCS-5-A
+*/
+#define BUTTON_PIN 4
+ void setup() {
+ pinMode(BUTTON_PIN, INPUT_PULLUP);
+ Serial.begin(115200);
+ }
+ void loop() {
+ if(digitalRead(BUTTON_PIN) == LOW)
+ Serial.println("Button Pressed");
+ else
+ Serial.println("Button Released");
+ }
